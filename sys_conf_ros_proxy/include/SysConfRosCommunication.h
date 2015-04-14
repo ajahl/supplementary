a98ba472c488366d82dfd6f2518ea6aa7880c31d
Nida Kizilaslan <n.kizilaslan@web.de>
2015-04-14 07:49:39 +0200

    -For testing ROS Communication(not usable) -add testconfigs

---
+++ sys_conf_ros_proxy/include/SysConfRosCommunication.h
@@ -0,0 +1,46 @@
+/*
+ * SysConfRosCommunication.h
+ *
+ *  Created on: 13.03.2015
+ *      Author: nida_bjk
+ */
+
+#ifndef SYSCONFROSCOMMUNICATION_H_
+#define SYSCONFROSCOMMUNICATION_H_
+
+
+#include "ISysConfCommunication.h"
+#include "ros/ros.h"
+
+#include "sys_conf_ros_proxy/ChangeAtr.h"
+
+using namespace supplementary;
+
+namespace sysConfRosProxy
+{
+	class SysConfRosCommunication: public supplementary::ISysConfCommunication
+	{
+		public:
+			SysConfRosCommunication(SystemConfig* sc);
+			virtual ~SysConfRosCommunication();
+
+			virtual void sendChangeAtr(ChangeAtr& ca);
+			virtual void handleChangeAtrRos(sys_conf_ros_proxy::ChangeAtrPtr scca);
+
+
+			virtual void tick();
+
+			virtual void startCommunication();
+			virtual void stopCommunication();
+		protected:
+			ros::NodeHandle* rosNode;
+			ros::AsyncSpinner* spinner;
+
+			ros::Publisher ChangeAtrPublisher;
+
+			bool isRunning;
+	};
+}
+
+
+#endif /* SYSCONFROSCOMMUNICATION_H_ */
